0c9dc401427f5eeb66ed480b883d50b306527640
Bartek Mis <bartek@studiosidekicks.com>
2019-01-04 20:11:49 +0100

    url to service worker updated

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -35,7 +35,7 @@
 
   if ('serviceWorker' in navigator &&
       (window.location.protocol === 'https:' || isLocalhost)) {
-    navigator.serviceWorker.register('../service-worker.js')
+    navigator.serviceWorker.register('service-worker.js')
     .then(function(registration) {
       // updatefound is fired if service-worker.js changes.
       registration.onupdatefound = function() {
